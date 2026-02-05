9edaee6274409f2c564be7481369f0b2dbf3f841
Brian K Preston <brian8181@gmail.com>
2026-02-05 05:04:15 +0000

    Auto-commit changes

--- doc/acp.sh
+++ doc/acp.sh
@@ -4,10 +4,10 @@ source $HOME/bin/color.sh
 
 PrintRed "\nAuto-committing changes to git repository...\n"
 git add .
-PrintYellow "Changes staged for commit.\n"
+PrintYellow "\nChanges staged for commit.\n"
 git commit -m "Auto-commit changes"
-PrintYellow "Changes committed.\n"
+PrintYellow "\nChanges committed.\n"
 git push
-PrintGreen "Changes pushed to remote repository.\n"
+PrintGreen "\nChanges pushed to remote repository.\n"
 git log --oneline -5
-PrintGreen "Last 5 commits displayed.\n"
+PrintGreen "\nLast 5 commits displayed.\n"
